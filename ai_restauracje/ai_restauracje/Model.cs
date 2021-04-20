04c588d3776ce9f2f19faafa8ab4e04427064734
Sorebit <cmbk90@gmail.com>
2021-04-20 17:37:56 +0200

    Add placeholder model

--- ai_restauracje/ai_restauracje/Model.cs
+++ ai_restauracje/ai_restauracje/Model.cs
@@ -6,8 +6,50 @@ using System.Threading.Tasks;
 
 namespace ai_restauracje
 {
-	class Model
+	public class Model
 	{
+		public List<string> AttributesList;
+		public List<Restaurant> Restaurants;
 
+		public Model(string filepath = "")
+        {
+			// TODO: Load JSON file
+
+			// TODO: Load AttributesList from JSON
+			AttributesList = new List<string>
+			{
+				"atrybut 1",
+				"atrrrrrrrr"
+			};
+
+			// TODO: Load from JSON
+			// coś jak Restaurants = JSON.LoadFile(class=List<Restaurant>, filepath);
+
+			Restaurants = new List<Restaurant>
+			{
+				new Restaurant("wielkie szyncle", "warszaw", AttributesList.Count),
+				new Restaurant("u szwejka", "warszawa", AttributesList.Count),
+				new Restaurant("u szwejka 2", "krakuf", AttributesList.Count),
+			};
+		}
 	}
+
+	public class Restaurant
+    {
+		public string Name;
+		public string Location;
+        public double[] Attributes;
+
+		public double Sim(Restaurant other)
+        {
+			return 0.0;
+        }
+
+		public Restaurant(string name, string location, int attributesCount)
+        {
+			Name = name;
+			Location = location;
+			Attributes = new double[attributesCount];
+        }
+    }
 }
